d1cad9d2be6aea463699dbd0abf399369e28f78b
Leonardorossato <leonardorossato21@gmail.com>
2023-08-18 16:23:07 -0300

    doing login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,13 +8,25 @@ class AuthController {
       if (!user) {
         const newUser = await User.create(req.body);
         await newUser.save();
-        return newUser;
+        return res.status(201).json(newUser);
       } else {
         throw new Error("Error in one of the user fields");
       }
     } catch (error) {
       throw new Error("User already registered.");
     }
+  };
+
+  static login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email: email });
+      if (user && (await user.isPasswordMatched(password))) {
+        return res.status(201).json(user);
+      } else {
+        throw new Error("Invalid Credentials");
+      }
+    } catch (error) {}
   };
 }
 
