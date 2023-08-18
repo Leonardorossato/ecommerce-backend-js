2cefaa5d734cee47f1ee6ca0195b7094cf3ba10f
Leonardorossato <leonardorossato21@gmail.com>
2023-08-18 13:59:29 -0300

    added envs to use

--- src/env/envoriment.js
+++ src/env/envoriment.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
-const APP_PORT = process.env.APP_PORT;
-const MONGO_URL = process.env.MONGO_URL;
-const JWT_SECRET = process.env.JWT_SECRET;
+const processEnv = {
+  APP_PORT: process.env.APP_PORT,
+  MONGO_URL: process.env.MONGO_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+};
 
-module.exports = { APP_PORT, MONGO_URL, JWT_SECRET };
+module.exports = processEnv;
