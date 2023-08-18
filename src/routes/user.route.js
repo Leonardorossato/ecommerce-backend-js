21fe4dad0ae237a08cfe231d1dc1d7e6cd6d651b
Leonardorossato <leonardorossato21@gmail.com>
2023-08-18 17:57:07 -0300

    update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const UserController = require("../controllers/user.controller");
 const router = express.Router();
+const UserController = require("../controllers/user.controller");
 
 router.get("/all", UserController.findAll);
 router.get("/:id", UserController.findOne);
