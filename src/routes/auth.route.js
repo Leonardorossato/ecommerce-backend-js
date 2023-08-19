9a59bd40b84c046d7de175070399d69e03f25c0d
Leonardorossato <leonardorossato21@gmail.com>
2023-08-19 09:04:36 -0300

    clear

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const AuthController = require("../controllers/auth.controller");
 
-
 router.post("/login", AuthController.login);
 router.post("/register", AuthController.register);
 
