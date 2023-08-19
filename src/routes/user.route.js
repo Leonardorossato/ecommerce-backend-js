58ca1683123020a7225323c0f3b2fb0ebbbd71b3
Leonardorossato <leonardorossato21@gmail.com>
2023-08-19 09:48:20 -0300

    update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/user.controller");
+const {
+  validateAuthToken,
+  adminAuthToken,
+  userAuthToken,
+} = require("../middleware/jwt.middleware");
 
-router.get("/all", UserController.findAll);
-router.get("/:id", UserController.findOne);
-router.put("/:id", UserController.update);
+router.get("/all", validateAuthToken, adminAuthToken, UserController.findAll);
+router.get("/:id", validateAuthToken, adminAuthToken, UserController.findOne);
+router.put("/:id", validateAuthToken, userAuthToken, UserController.update);
+router.put(
+  "/block/:id",
+  validateAuthToken,
+  adminAuthToken,
+  UserController.blockUser
+);
+router.put(
+  "/unblock/:id",
+  validateAuthToken,
+  adminAuthToken,
+  UserController.unblockUser
+);
 router.delete("/:id", UserController.delete);
 
 module.exports = router;
