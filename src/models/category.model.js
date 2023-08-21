f2576ef590cf7434db24157a35ffa348b33d4eaa
Leonardorossato <leonardorossato21@gmail.com>
2023-08-21 14:10:56 -0300

    feat: create category.model.js

---
+++ src/models/category.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const categorySchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true, index: true },
+  },
+  { timestamps: true }
+);
+
+const Category = mongoose.model("Category", categorySchema);
+module.exports = Category;
