a974e6e14992e880a4dadaf6cf86c3b52ef3c703
Leonardorossato <leonardorossato21@gmail.com>
2023-08-20 21:37:02 -0300

    update blog.route.js

--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const BlogController = require("../controllers/blog.controller");
+const {
+  validateAuthToken,
+  adminAuthToken,
+} = require("../middleware/jwt.middleware");
 const router = express.Router();
 
-router.get("/all", BlogController.findAll);
-router.post("create", BlogController.create);
+router.get("/all", validateAuthToken, adminAuthToken, BlogController.findAll);
+router.get("views/:id", validateAuthToken, adminAuthToken, BlogController.getByNumbersOfViews);
+router.post(
+  "/create",
+  validateAuthToken,
+  adminAuthToken,
+  BlogController.create
+);
+router.put("/:id", validateAuthToken, adminAuthToken, BlogController.update);
+router.delete("/:id", validateAuthToken, adminAuthToken, BlogController.delete);
 
 module.exports = router;
