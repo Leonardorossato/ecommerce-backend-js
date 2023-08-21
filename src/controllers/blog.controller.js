da64f908c82c1a4919404b9ff68091100e0050d8
Leonardorossato <leonardorossato21@gmail.com>
2023-08-20 21:37:04 -0300

    update blog.controller.js

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -1,4 +1,4 @@
-const Blog = require("../models/blog.mode");
+const Blog = require("../models/blog.model");
 
 class BlogController {
   static create = async (req, res) => {
@@ -17,6 +17,60 @@ class BlogController {
       return res.status(200).json(blog);
     } catch (error) {
       return res.status(500).json({ message: "Error to find all blogs." });
+    }
+  };
+
+  static getByNumbersOfViews = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const blog = await Blog.findById(id);
+      if (!blog) {
+        return res
+          .status(404)
+          .json({ message: `Blog with id ${id} not found` });
+      }
+      const views = await Blog.findByIdAndUpdate(
+        id,
+        { $inc: { numberOfViews: 1 } },
+        { new: true }
+      );
+      return res.status(200).json(blog, views);
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ message: `This blog has a error: ${error.message}` });
+    }
+  };
+
+  static update = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const blog = await Blog.findById(id);
+      if (!blog) {
+        return res
+          .status(404)
+          .json({ message: `Blog with id ${id} not found` });
+      }
+      await Blog.findByIdAndUpdate(id, req.body, { new: true });
+      return res.status(200).json({ message: "Blog updated successfully." });
+    } catch (error) {
+      return res.status(500).json({ message: "Error updating this blog." });
+    }
+  };
+
+  static delete = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const blog = await Blog.findById(id);
+      if (!blog) {
+        return res
+          .status(404)
+          .json({ message: `Blog with id ${id} not found` });
+      }
+      await Blog.findByIdAndDelete(id);
+      return res.status(200).json({ message: "Blog deleted successfully" });
+    } catch (error) {
+      return res.status(500).json({ message: "Error while deleting blog" });
     }
   };
 }
