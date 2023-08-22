f6ee46af27fb21f2a4df23219c3ca32edf6e7da1
Leonardorossato <leonardorossato21@gmail.com>
2023-08-21 21:02:53 -0300

    addes more routes and all working

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const ProductController = require("../controllers/product.controller");
+const {
+  validateAuthToken,
+  userAuthToken,
+  adminAuthToken,
+} = require("../middleware/jwt.middleware");
 
 router.get("/all", ProductController.findAll);
 router.get("/:id", ProductController.findOne);
 router.get("/", ProductController.queryPagination);
-router.post("/create", ProductController.create);
-router.put("/:id", ProductController.update);
-router.delete("/:id", ProductController.delete);
+router.post(
+  "/create",
+  validateAuthToken,
+  adminAuthToken,
+  ProductController.create
+);
+router.put(
+  "/wishiList",
+  validateAuthToken,
+  userAuthToken,
+  ProductController.addToWishiList
+);
+router.put(
+  "/rating",
+  validateAuthToken,
+  userAuthToken,
+  ProductController.ratingProduct
+);
+router.put("/:id", validateAuthToken, adminAuthToken, ProductController.update);
+router.delete(
+  "/:id",
+  validateAuthToken,
+  adminAuthToken,
+  ProductController.delete
+);
 
 module.exports = router;
