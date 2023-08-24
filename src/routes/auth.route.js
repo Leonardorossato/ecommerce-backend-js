e08f2d5331425bc8c8d1a51f37a1c812ddafb577
Leonardorossato <leonardorossato21@gmail.com>
2023-08-24 14:16:22 -0300

    update auth.route.js

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const AuthController = require("../controllers/auth.controller");
 
 router.post("/login", AuthController.login);
+router.post("/admin-login", AuthController.loginAdmin);
 router.get("/refresh", AuthController.handleRefreshToken);
 router.get("/logout", AuthController.logOut);
 router.post("/register", AuthController.register);
