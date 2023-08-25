297faf2d02353ef36e4369804a42894702166abe
Leonardorossato <leonardorossato21@gmail.com>
2023-08-25 11:17:03 -0300

    added the final routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -7,11 +7,39 @@ const {
   userAuthToken,
 } = require("../middleware/jwt.middleware");
 
+router.get(
+  "/all-order",
+  validateAuthToken,
+  userAuthToken,
+  UserController.getAllCarts
+);
+
+router.get(
+  "/all-cart",
+  validateAuthToken,
+  userAuthToken,
+  UserController.getAllCarts
+);
+
+//Get admin route
 router.get("/all", validateAuthToken, adminAuthToken, UserController.findAll);
-router.post("/forgot-password", UserController.forgotPassword);
 router.get("/:id", validateAuthToken, adminAuthToken, UserController.findOne);
+
+//Post routes
+router.post("/cash-order", validateAuthToken, UserController.createOrder);
+router.post("/apply-cupom", validateAuthToken, UserController.applyCupom);
+router.post("/forgot-password", UserController.forgotPassword);
+router.post("/add-cart", validateAuthToken, userAuthToken, UserController.cart);
+
+//Put routes
+router.put(
+  "/update-order/:id",
+  validateAuthToken,
+  adminAuthToken,
+  UserController.updateOrderStatus
+);
 router.put("/reset-password/:token", UserController.resetPassword);
-router.put("/address",validateAuthToken, UserController.saveAddress);
+router.put("/address", validateAuthToken, UserController.saveAddress);
 router.put("/password", validateAuthToken, UserController.updatePassword);
 router.put("/:id", validateAuthToken, userAuthToken, UserController.update);
 router.put(
@@ -25,6 +53,14 @@ router.put(
   validateAuthToken,
   adminAuthToken,
   UserController.unblockUser
+);
+
+//Delete route
+router.delete(
+  "/empty-cart",
+  validateAuthToken,
+  userAuthToken,
+  UserController.emptyCart
 );
 router.delete("/:id", UserController.delete);
 
